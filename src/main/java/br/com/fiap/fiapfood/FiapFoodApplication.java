dd0ef7df4c678d46767c93a22f961f0e95570067
Diego Nassula <diego@digihub.com.br>
2024-04-15 19:43:12 -0300

    Commit Inicial

---
+++ src/main/java/br/com/fiap/fiapfood/FiapFoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiapfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/fiapfood/FiapFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiapfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
