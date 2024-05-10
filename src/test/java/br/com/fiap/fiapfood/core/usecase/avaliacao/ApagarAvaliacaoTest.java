986e76488520d67c9bd8db94ec7fa2942d5e491b
brunorosselli <bruno.rosselli@hotmail.com>
2024-05-10 16:13:34 -0300

    Revert "delete worng test"
    This reverts commit 5826bb7ce53b7087ce86d6ff48bfe130f500f27d.

---
+++ src/test/java/br/com/fiap/fiapfood/core/usecase/avaliacao/ApagarAvaliacaoTest.java
@@ -0,0 +1,26 @@
+package br.com.fiap.fiapfood.core.usecase.avaliacao;
+
+import br.com.fiap.fiapfood.core.gateways.AvaliacaoRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+
+class ApagarAvaliacaoTest {
+
+    /*Teste de Apagar*/
+    @Test
+    void testApagarAvaliacao() {
+        // Arrange
+        AvaliacaoRepository mockRepository = Mockito.mock(AvaliacaoRepository.class);
+        ApagarAvaliacao apagarAvaliacao = new ApagarAvaliacao(mockRepository);
+        Long avaliacaoId = 1L;
+
+        // Act
+        apagarAvaliacao.call(avaliacaoId);
+
+        // Assert
+        Mockito.verify(mockRepository, times(1)).apagar(avaliacaoId);
+    }
+}
