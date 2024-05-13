aacd190b9f224d376303d86c03df7b64174db875
Bruno Araujo <brunosousa357@gmail.com>
2024-05-13 20:15:36 -0300

    adicionando data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO USUARIO (id, nome, email, cpf)
+VALUES
+    ('1', 'Bruno', 'bruno@fiap.com', '01234567890'),
+    ('2', 'Milton', 'milton@fiap.com', '01234567891'),
+    ('3', 'Diego', 'diego@fiap.com', '01234567892'),
+    ('4', 'Toquinho', 'toquinho@fiap.com', '01234567893'),
+    ('5', 'Robson', 'robson@fiap.com', '01234567894');
